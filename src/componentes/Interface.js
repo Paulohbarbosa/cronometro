f1f154a1763c801a27c6cfa7a954105bbec59b93
Paulo Barbosa <paulobarbosah@hotmail.com>
2020-10-17 19:54:50 -0300

    teste de função

--- src/componentes/Interface.js
+++ src/componentes/Interface.js
@@ -4,23 +4,25 @@ import Temporizador from '../temporizador/Temporizador';
 import Relogio from '../relogio/Relogio';
 import Cronometro from '../cronometro/Cronometro';
 import { Alarm, Clock, Stopwatch } from 'react-bootstrap-icons';
-//import '../App.css';
-//import Clock  from 'react-bootstrap-icons';
+import Saudacao from './Saudaçao';
 
 const Interface = (props) => (
-    
+
     <Container>
         <Row className="justify-content-md-center">
             <Col md="auto">
                 <Tabs defaultActiveKey="Relógio" id="uncontrolled-tab-example">
-                    <Tab eventKey="Relógio" title={<Clock color="royalblue" size={50}/>}>
-                        <Relogio/>
+                    <Tab eventKey="Relógio" title={<Clock color="royalblue" size={50} />}>
+                        <Relogio />
                     </Tab>
-                    <Tab eventKey="Temporizador" title={<Alarm color="royalblue" size={50}/>}>
-                        <Temporizador/>
+                    <Tab eventKey="Temporizador" title={<Alarm color="royalblue" size={50} />}>
+                        <Temporizador />
                     </Tab>
-                    <Tab eventKey="Cronometro" title={<Stopwatch color="royalblue" size={50}/>}>
+                    <Tab eventKey="Cronometro" title={<Stopwatch color="royalblue" size={50} />}>
                         <Cronometro />
+                    </Tab>
+                    <Tab eventKey="test" title="Test">
+                        <Saudacao tipo="bom dia" nome="João" />
                     </Tab>
                 </Tabs>
             </Col>
